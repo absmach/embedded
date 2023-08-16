2b9f09e3a64e34002c96e40971fb490815f4edc3
1998-felix <felix.gateru@gmail.com>
2023-08-16 16:38:54 +0300

    Cleaning up
    Signed-off-by: 1998-felix <felix.gateru@gmail.com>

--- clients/ESP32/include/config.h
+++ clients/ESP32/include/config.h
@@ -5,18 +5,18 @@
 
 struct broker_details
 {
-    const char *mf_thing_id = "845c3b52-583b-45d2-be14-f46a1f7f5d3f";
-    const char *mf_thing_pass = "123456789";
-    const char *mf_channel_id = "1eab858c-7407-4c3e-97b2-f357ee95a5b8";
-    const char *mf_topic = "channels/1eab858c-7407-4c3e-97b2-f357ee95a5b8/messages";
+    const char *mf_thing_id = "";
+    const char *mf_thing_pass = "";
+    const char *mf_channel_id = "";
+    const char *mf_topic = "";
 } br_config;
 
 struct wifi_details
 {
-    const char *wifi_ssid = "OH_LAN";
-    const char *wifi_pass = "0799973955";
+    const char *wifi_ssid = "";
+    const char *wifi_pass = "";
 } wifi_config;
 
-IPAddress server(192,168,0,130);
+IPAddress server(000,000,000,000);
 
 #endif
