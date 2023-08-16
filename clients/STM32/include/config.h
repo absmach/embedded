5ef23c8fb7726cbd8f4fd36d4757308f47700d3c
1998-felix <felix.gateru@gmail.com>
2023-08-16 16:40:49 +0300

    Add STM32 clinet
    Signed-off-by: 1998-felix <felix.gateru@gmail.com>

---
+++ clients/STM32/include/config.h
@@ -0,0 +1,22 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <WiFiEsp.h>
+
+struct broker_details
+{
+    const char *mf_thing_id = "";
+    const char *mf_thing_pass = "";
+    const char *mf_channel_id = "";
+    const char *mf_topic = "";
+} br_config;
+
+struct wifi_details
+{
+    const char *wifi_ssid = "";
+    const char *wifi_pass = "";
+} wifi_config;
+
+IPAddress server(000,000,000,000);
+
+#endif
