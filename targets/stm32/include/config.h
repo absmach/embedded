6d473ec4d0ffcfc1b1149cf2bdd9677c2305016a
1998-felix <felix.gateru@gmail.com>
2023-08-21 06:08:39 +0300

    Remove personal config
    Signed-off-by: 1998-felix <felix.gateru@gmail.com>

--- targets/stm32/include/config.h
+++ targets/stm32/include/config.h
@@ -4,9 +4,9 @@
 
 struct BrokerDetails
 {
-    const char *mfThingId = "64c2fc54-7d5c-4b4f-903c-e628ff7aa23e";
-    const char *mfThingPass = "24eb50a8-f281-4789-9435-1d4909086da4";
-    const char *mfChannelId = "1eab858c-7407-4c3e-97b2-f357ee95a5b8";
+    const char *mfThingId = "";
+    const char *mfThingPass = "";
+    const char *mfChannelId = "";
     char mfTopic[150];
     BrokerDetails()
     {
@@ -20,12 +20,12 @@ struct BrokerDetails
 
 struct WifiDetails
 {
-    const char *wifiSsid = "Octavifi";
-    const char *wifiPass = "Unic0rn_2030";
+    const char *wifiSsid = "";
+    const char *wifiPass = "";
 } wifiConfig;
 
-// IPAddress server(192, 168, 100, 122);
+// IPAddress server(192, 168, 000, 000);
 
-const char *server = "felix-Aspire-AV15-51.local";
+const char *server = "";
 
 #endif
