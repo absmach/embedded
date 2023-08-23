fbbbf7a7321915b871dbfd5365a831272d892aa7
1998-felix <felix.gateru@gmail.com>
2023-08-23 13:49:41 +0300

    Add esp32 mqtt capability
    Signed-off-by: 1998-felix <felix.gateru@gmail.com>

---
+++ targets/esp32/mqtt/include/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char *mfThingId = " ";
+const char *mfThingPass = " ";
+const char *mfChannelId = " ";
+char mfTopic[150];
+
+const char *server = " ";
+
+#endif
