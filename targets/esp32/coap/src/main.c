99b3b963ceed1073d6f6d2ec50395a5d4d73ef15
felix.gateru <felix.gateru@gmail.com>
2023-10-13 13:15:08 +0300

    Append CoAP config
    Signed-off-by: felix.gateru <felix.gateru@gmail.com>

--- targets/esp32/coap/src/main.c
+++ targets/esp32/coap/src/main.c
@@ -12,16 +12,26 @@
 #include "nvs_flash.h"
 
 #include "protocol_examples_common.h"
-
 #include "coap3/coap.h"
+#include "config.h"
+#include "cnetwork.h"
 
-#define COAP_DEFAULT_TIME_SEC 60
-
-const static char *TAG = "CoAP_client";
+const static char *TAG = CLIENTID;
 
 static int resp_wait = 1;
 static coap_optlist_t *optlist = NULL;
 static int wait_ms;
+
+
+void format_mainflux_message_topic(void)
+{
+    const char *_preId = "channels/";
+    const char *_postId = "/messages";
+    strcpy(mfTopic, _preId);
+    strcat(mfTopic, mfChannelId);
+    strcat(mfTopic, _postId);
+}
+
 
 static coap_response_t message_handler(coap_session_t *session,
                                        const coap_pdu_t *sent,
@@ -79,73 +89,11 @@ static void coap_log_handler(coap_log_t level, const char *message)
     }
 }
 
-static int coap_build_optlist(coap_uri_t *uri)
+static void coap_client(void *p)
 {
-#define BUFSIZE 40
-    unsigned char _buf[BUFSIZE];
-    unsigned char *buf;
-    size_t buflen;
-    int res;
-
-    optlist = NULL;
-
-    if (uri->scheme == COAP_URI_SCHEME_COAPS && !coap_dtls_is_supported())
-    {
-        ESP_LOGE(TAG, "MbedTLS DTLS Client Mode not configured");
-        return 0;
-    }
-    if (uri->scheme == COAP_URI_SCHEME_COAPS_TCP && !coap_tls_is_supported())
-    {
-        ESP_LOGE(TAG, "MbedTLS TLS Client Mode not configured");
-        return 0;
-    }
-    if (uri->scheme == COAP_URI_SCHEME_COAP_TCP && !coap_tcp_is_supported())
-    {
-        ESP_LOGE(TAG, "TCP Client Mode not configured");
-        return 0;
-    }
-
-    if (uri->path.length)
-    {
-        buflen = BUFSIZE;
-        buf = _buf;
-        res = coap_split_path(uri->path.s, uri->path.length, buf, &buflen);
-
-        while (res--)
-        {
-            coap_insert_optlist(&optlist,
-                                coap_new_optlist(COAP_OPTION_URI_PATH,
-                                                 coap_opt_length(buf),
-                                                 coap_opt_value(buf)));
-
-            buf += coap_opt_size(buf);
-        }
-    }
-
-    if (uri->query.length)
-    {
-        buflen = BUFSIZE;
-        buf = _buf;
-        res = coap_split_query(uri->query.s, uri->query.length, buf, &buflen);
-
-        while (res--)
-        {
-            coap_insert_optlist(&optlist,
-                                coap_new_optlist(COAP_OPTION_URI_QUERY,
-                                                 coap_opt_length(buf),
-                                                 coap_opt_value(buf)));
-
-            buf += coap_opt_size(buf);
-        }
-    }
-    return 1;
-}
-
-static void coap_example_client(void *p)
-{
-    coap_address_t dst_addr;
-    static coap_uri_t uri;
-    const char *server_uri = COAP_DEFAULT_DEMO_URI;
+    coap_address_t uri;
+    static coap_uri_t dst_addr;
+    const char *server_uri = server;
     coap_context_t *ctx = NULL;
     coap_session_t *session = NULL;
     coap_pdu_t *request = NULL;
@@ -155,14 +103,11 @@ static void coap_example_client(void *p)
     coap_proto_t proto;
     char tmpbuf[INET6_ADDRSTRLEN];
 
-    /* Initialize libcoap library */
     coap_startup();
 
-    /* Set up the CoAP logging */
     coap_set_log_handler(coap_log_handler);
-    coap_set_log_level(EXAMPLE_COAP_LOG_DEFAULT_LEVEL);
+    coap_set_log_level(COAP_LOG_DEFAULT_LEVEL);
 
-    /* Set up the CoAP context */
     ctx = coap_new_context(NULL);
     if (!ctx)
     {
@@ -179,10 +124,6 @@ static void coap_example_client(void *p)
         ESP_LOGE(TAG, "CoAP server uri %s error", server_uri);
         goto clean_up;
     }
-    if (!coap_build_optlist(&uri))
-    {
-        goto clean_up;
-    }
 
     info_list = coap_resolve_address_info(&uri.host, uri.port, uri.port,
                                           uri.port, uri.port,
@@ -193,52 +134,19 @@ static void coap_example_client(void *p)
     if (info_list == NULL)
     {
         ESP_LOGE(TAG, "failed to resolve address");
-        goto clean_up;
+        clean_up();
     }
     proto = info_list->proto;
     memcpy(&dst_addr, &info_list->addr, sizeof(dst_addr));
     coap_free_address_info(info_list);
-
-    /* This is to keep the test suites happy */
-    coap_print_ip_addr(&dst_addr, tmpbuf, sizeof(tmpbuf));
-    ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", tmpbuf);
-
-    /*
-     * Note that if the URI starts with just coap:// (not coaps://) the
-     * session will still be plain text.
-     */
-    if (uri.scheme == COAP_URI_SCHEME_COAPS || uri.scheme == COAP_URI_SCHEME_COAPS_TCP || uri.scheme == COAP_URI_SCHEME_COAPS_WS)
-    {
-#ifndef CONFIG_MBEDTLS_TLS_CLIENT
-        ESP_LOGE(TAG, "MbedTLS (D)TLS Client Mode not configured");
-        goto clean_up;
-#endif /* CONFIG_MBEDTLS_TLS_CLIENT */
-
-#ifdef CONFIG_COAP_MBEDTLS_PSK
-        session = coap_start_psk_session(ctx, &dst_addr, &uri, proto);
-#endif /* CONFIG_COAP_MBEDTLS_PSK */
-
-#ifdef CONFIG_COAP_MBEDTLS_PKI
-        session = coap_start_pki_session(ctx, &dst_addr, &uri, proto);
-#endif /* CONFIG_COAP_MBEDTLS_PKI */
-    }
-    else
-    {
-        session = coap_new_client_session(ctx, NULL, &dst_addr, proto);
-    }
+    session = coap_new_client_session(ctx, NULL, &dst_addr, proto);
     if (!session)
     {
         ESP_LOGE(TAG, "coap_new_client_session() failed");
-        goto clean_up;
+        clean_up();
     }
-#ifdef CONFIG_COAP_WEBSOCKETS
-    if (proto == COAP_PROTO_WS || proto == COAP_PROTO_WSS)
-    {
-        coap_ws_set_host_request(session, &uri.host);
-    }
-#endif /* CONFIG_COAP_WEBSOCKETS */
 
-    while (1)
+    while (true)
     {
         request = coap_new_pdu(coap_is_mcast(&dst_addr) ? COAP_MESSAGE_NON : COAP_MESSAGE_CON,
                                COAP_REQUEST_CODE_GET, session);
@@ -247,7 +155,6 @@ static void coap_example_client(void *p)
             ESP_LOGE(TAG, "coap_new_pdu() failed");
             goto clean_up;
         }
-        /* Add in an unique token */
         coap_session_new_token(session, &tokenlength, token);
         coap_add_token(request, tokenlength, token);
         coap_add_optlist_pdu(request, &optlist);
@@ -255,7 +162,7 @@ static void coap_example_client(void *p)
         resp_wait = 1;
         coap_send(session, request);
 
-        wait_ms = COAP_DEFAULT_TIME_SEC * 1000;
+        wait_ms = COAP_DEFAULT_TIME_SEC * MS_COUNT;
 
         while (resp_wait)
         {
@@ -280,13 +187,9 @@ static void coap_example_client(void *p)
         }
         ESP_LOGI(TAG, "Starting again!");
     }
-
-clean_up:
-    if (optlist)
-    {
-        coap_delete_optlist(optlist);
-        optlist = NULL;
-    }
+}
+void clean_up()
+{
     if (session)
     {
         coap_session_release(session);
@@ -300,13 +203,12 @@ clean_up:
     ESP_LOGI(TAG, "Finished");
     vTaskDelete(NULL);
 }
-
 void app_main(void)
 {
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    ESP_ERROR_CHECK(example_connect());
+    ESP_ERROR_CHECK(wifi_init_softap());
 
-    xTaskCreate(coap_example_client, "coap", 8 * 1024, NULL, 5, NULL);
+    xTaskCreate(coap_client, "coap", COAP_BUFFSIZE, NULL, COAP_TIMEOUT, NULL);
 }
