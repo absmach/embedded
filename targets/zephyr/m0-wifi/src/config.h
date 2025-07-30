b48cc2ff1cde68263757131cb44e4b663894bfac
JonesKisaka <kisakathejones@gmail.com>
2025-07-30 10:11:58 +0300

    Wifi code update

---
+++ targets/zephyr/m0-wifi/src/config.h
@@ -0,0 +1,21 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define MACSTR "%02X:%02X:%02X:%02X:%02X:%02X"
+
+#define NET_EVENT_WIFI_MASK                                                                        \
+	(NET_EVENT_WIFI_CONNECT_RESULT | NET_EVENT_WIFI_DISCONNECT_RESULT |                        \
+	 NET_EVENT_WIFI_AP_ENABLE_RESULT | NET_EVENT_WIFI_AP_DISABLE_RESULT |                      \
+	 NET_EVENT_WIFI_AP_STA_CONNECTED | NET_EVENT_WIFI_AP_STA_DISCONNECTED)
+
+/* AP Mode Configuration */
+#define WIFI_AP_SSID       "M0-AP"
+#define WIFI_AP_PSK        ""
+#define WIFI_AP_IP_ADDRESS "192.168.4.1"
+#define WIFI_AP_NETMASK    "255.255.255.0"
+
+/* STA Mode Configuration */
+#define WIFI_SSID "SSID"     /* Replace `SSID` with WiFi ssid. */
+#define WIFI_PSK  "PASSWORD" /* Replace `PASSWORD` with Router password. */
+
+#endif
