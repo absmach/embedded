fd927cf1345cc3fc5ad0fa085d1cedbeca33e9ee
JonesKisaka <kisakathejones@gmail.com>
2025-07-31 12:07:55 +0300

    Updated code after rebase

---
+++ targets/zephyr/m0-mqtt/src/main.c
@@ -0,0 +1,463 @@
+#include <string.h>
+#include <errno.h>
+
+#include <zephyr/logging/log.h>
+#include <zephyr/kernel.h>
+#include <zephyr/net/socket.h>
+#include <zephyr/net/mqtt.h>
+#include <zephyr/random/random.h>
+#include <zephyr/app_memory/app_memdomain.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/net_event.h>
+#include "config.h"
+
+K_APPMEM_PARTITION_DEFINE(app_partition);
+#define APP_BMEM K_APP_BMEM(app_partition)
+#define APP_DMEM K_APP_DMEM(app_partition)
+#define SUCCESS_OR_EXIT(rc) success_or_exit(rc)
+#define SUCCESS_OR_BREAK(rc) success_or_break(rc)
+#define PROG_DELAY 5000
+#define APP_MQTT_BUFFER_SIZE 1024 // Replace 1024 with the desired buffer size
+#define SERVER_PORT 1883
+#define APP_CONNECT_TRIES 3
+#define APP_CONNECT_TIMEOUT_MS 1000
+#define APP_SLEEP_MSECS 1000
+#define MQTT_CLIENTID "d7489283-7dd7-4ea1-8073-ab05e0aac2ed" // Replace with your actual client ID
+#define WIFI_SSID "Kisaka the Jones" // Replace with your WiFi SSID
+#define WIFI_PSK "Panoholic11688" // Replace with your WiFi Password
+
+LOG_MODULE_REGISTER(mqtt, LOG_LEVEL_DBG);
+
+static APP_BMEM uint8_t rx_buffer[APP_MQTT_BUFFER_SIZE];
+static APP_BMEM uint8_t tx_buffer[APP_MQTT_BUFFER_SIZE];
+
+static APP_BMEM struct mqtt_client client_ctx;
+
+static APP_BMEM struct sockaddr_storage broker_addr;
+
+static APP_BMEM struct zsock_pollfd fds[1];
+static APP_BMEM int nfds;
+
+static APP_BMEM bool connected;
+
+static void prepare_fds(struct mqtt_client *client)
+{
+	if (client->transport.type == MQTT_TRANSPORT_NON_SECURE)
+	{
+		fds[0].fd = client->transport.tcp.sock;
+	}
+	fds[0].events = ZSOCK_POLLIN;
+	nfds = 1;
+}
+
+static void clear_fds(void)
+{
+	nfds = 0;
+}
+
+static int wait(int timeout)
+{
+	int ret = 0;
+
+	if (nfds > 0)
+	{
+		ret = zsock_poll(fds, nfds, timeout);
+		if (ret < 0)
+		{
+			LOG_ERR("poll error: %d", errno);
+		}
+	}
+
+	return ret;
+}
+
+void mqtt_evt_handler(struct mqtt_client *const client,
+					  const struct mqtt_evt *evt)
+{
+	int err;
+
+	switch (evt->type)
+	{
+	case MQTT_EVT_CONNACK:
+		if (evt->result != 0)
+		{
+			LOG_ERR("MQTT connect failed %d", evt->result);
+			break;
+		}
+
+		connected = true;
+		LOG_INF("MQTT client connected!");
+
+		break;
+
+	case MQTT_EVT_DISCONNECT:
+		LOG_INF("MQTT client disconnected %d", evt->result);
+
+		connected = false;
+		clear_fds();
+
+		break;
+
+	case MQTT_EVT_PUBACK:
+		if (evt->result != 0)
+		{
+			LOG_ERR("MQTT PUBACK error %d", evt->result);
+			break;
+		}
+
+		LOG_INF("PUBACK packet id: %u", evt->param.puback.message_id);
+
+		break;
+
+	case MQTT_EVT_PUBREC:
+		if (evt->result != 0)
+		{
+			LOG_ERR("MQTT PUBREC error %d", evt->result);
+			break;
+		}
+
+		LOG_INF("PUBREC packet id: %u", evt->param.pubrec.message_id);
+
+		const struct mqtt_pubrel_param rel_param = {
+			.message_id = evt->param.pubrec.message_id};
+
+		err = mqtt_publish_qos2_release(client, &rel_param);
+		if (err != 0)
+		{
+			LOG_ERR("Failed to send MQTT PUBREL: %d", err);
+		}
+
+		break;
+
+	case MQTT_EVT_PUBCOMP:
+		if (evt->result != 0)
+		{
+			LOG_ERR("MQTT PUBCOMP error %d", evt->result);
+			break;
+		}
+
+		LOG_INF("PUBCOMP packet id: %u",
+				evt->param.pubcomp.message_id);
+
+		break;
+
+	case MQTT_EVT_PINGRESP:
+		LOG_INF("PINGRESP packet");
+		break;
+
+	default:
+		break;
+	}
+}
+
+static char *get_mqtt_payload(enum mqtt_qos qos)
+{
+	static APP_DMEM char payload[] = "{'message':'hello'}";
+
+	payload[strlen(payload) - 1] = '0' + qos;
+	return payload;
+}
+
+static char *get_mqtt_topic(void)
+{
+	const char *_preId = "m/";
+	const char *_postId = "/esp32c6In";
+	strcpy(mgTopic, _preId);
+	strcat(mgTopic, mgDomainId);
+	strcat(mgTopic, "/c/");
+	strcat(mgTopic, mgChannelId);
+	strcat(mgTopic, _postId);
+	return mgTopic;
+}
+
+static int publish(struct mqtt_client *client, enum mqtt_qos qos)
+{
+	struct mqtt_publish_param param;
+
+	param.message.topic.qos = qos;
+	param.message.topic.topic.utf8 = (uint8_t *)get_mqtt_topic();
+	param.message.topic.topic.size =
+		strlen(param.message.topic.topic.utf8);
+	param.message.payload.data = get_mqtt_payload(qos);
+	param.message.payload.len =
+		strlen(param.message.payload.data);
+	param.message_id = sys_rand32_get();
+	param.dup_flag = 0U;
+	param.retain_flag = 0U;
+
+	return mqtt_publish(client, &param);
+}
+
+#define RC_STR(rc) ((rc) == 0 ? "OK" : "ERROR")
+
+#define PRINT_RESULT(func, rc) \
+	LOG_INF("%s: %d <%s>", (func), rc, RC_STR(rc))
+
+static void broker_init(void)
+{
+	struct sockaddr_in *broker4 = (struct sockaddr_in *)&broker_addr;
+
+	broker4->sin_family = AF_INET;
+	broker4->sin_port = htons(SERVER_PORT);
+	zsock_inet_pton(AF_INET, server, &broker4->sin_addr);
+}
+
+static void client_init(struct mqtt_client *client)
+{
+	mqtt_client_init(client);
+
+	broker_init();
+
+	/* MQTT client configuration */
+	client->broker = &broker_addr;
+	client->evt_cb = mqtt_evt_handler;
+	client->client_id.utf8 = (uint8_t *)MQTT_CLIENTID;
+	client->client_id.size = strlen(MQTT_CLIENTID);
+	client->password = mgThingKey;
+	client->user_name = mgThingId;
+	client->protocol_version = MQTT_VERSION_3_1_1;
+
+	/* MQTT buffers configuration */
+	client->rx_buf = rx_buffer;
+	client->rx_buf_size = sizeof(rx_buffer);
+	client->tx_buf = tx_buffer;
+	client->tx_buf_size = sizeof(tx_buffer);
+	client->transport.type = MQTT_TRANSPORT_NON_SECURE;
+}
+
+static int try_to_connect(struct mqtt_client *client)
+{
+	int rc, i = 0;
+
+	while (i++ < APP_CONNECT_TRIES && !connected)
+	{
+
+		client_init(client);
+
+		rc = mqtt_connect(client);
+		if (rc != 0)
+		{
+			PRINT_RESULT("mqtt_connect", rc);
+			k_sleep(K_MSEC(APP_SLEEP_MSECS));
+			continue;
+		}
+
+		prepare_fds(client);
+
+		if (wait(APP_CONNECT_TIMEOUT_MS))
+		{
+			mqtt_input(client);
+		}
+
+		if (!connected)
+		{
+			mqtt_abort(client);
+		}
+	}
+
+	if (connected)
+	{
+		return 0;
+	}
+
+	return -EINVAL;
+}
+
+static int process_mqtt_and_sleep(struct mqtt_client *client, int timeout)
+{
+	int64_t remaining = timeout;
+	int64_t start_time = k_uptime_get();
+	int rc;
+
+	while (remaining > 0 && connected)
+	{
+		if (wait(remaining))
+		{
+			rc = mqtt_input(client);
+			if (rc != 0)
+			{
+				PRINT_RESULT("mqtt_input", rc);
+				return rc;
+			}
+		}
+
+		rc = mqtt_live(client);
+		if (rc != 0 && rc != -EAGAIN)
+		{
+			PRINT_RESULT("mqtt_live", rc);
+			return rc;
+		}
+		else if (rc == 0)
+		{
+			rc = mqtt_input(client);
+			if (rc != 0)
+			{
+				PRINT_RESULT("mqtt_input", rc);
+				return rc;
+			}
+		}
+
+		remaining = timeout + start_time - k_uptime_get();
+	}
+
+	return 0;
+}
+
+int success_or_exit(int rc)
+{
+	if (rc != 0)
+	{
+		return 1;
+	}
+
+	return 0;
+}
+
+void success_or_break(int rc)
+{
+	if (rc != 0)
+	{
+		return;
+	}
+}
+
+static int publisher(void)
+{
+	int i, rc, r = 0;
+
+	LOG_INF("attempting to connect: ");
+	rc = try_to_connect(&client_ctx);
+	PRINT_RESULT("try_to_connect", rc);
+	SUCCESS_OR_EXIT(rc);
+
+	i = 0;
+	while (i++ < CONFIG_NET_SAMPLE_APP_MAX_ITERATIONS && connected)
+	{
+		r = -1;
+
+		rc = mqtt_ping(&client_ctx);
+		PRINT_RESULT("mqtt_ping", rc);
+		SUCCESS_OR_BREAK(rc);
+
+		rc = process_mqtt_and_sleep(&client_ctx, APP_SLEEP_MSECS);
+		SUCCESS_OR_BREAK(rc);
+
+		rc = publish(&client_ctx, MQTT_QOS_0_AT_MOST_ONCE);
+		PRINT_RESULT("mqtt_publish", rc);
+		SUCCESS_OR_BREAK(rc);
+
+		rc = process_mqtt_and_sleep(&client_ctx, APP_SLEEP_MSECS);
+		SUCCESS_OR_BREAK(rc);
+
+		rc = publish(&client_ctx, MQTT_QOS_1_AT_LEAST_ONCE);
+		PRINT_RESULT("mqtt_publish", rc);
+		SUCCESS_OR_BREAK(rc);
+
+		rc = process_mqtt_and_sleep(&client_ctx, APP_SLEEP_MSECS);
+		SUCCESS_OR_BREAK(rc);
+
+		rc = publish(&client_ctx, MQTT_QOS_2_EXACTLY_ONCE);
+		PRINT_RESULT("mqtt_publish", rc);
+		SUCCESS_OR_BREAK(rc);
+
+		rc = process_mqtt_and_sleep(&client_ctx, APP_SLEEP_MSECS);
+		SUCCESS_OR_BREAK(rc);
+
+		r = 0;
+	}
+
+	rc = mqtt_disconnect(&client_ctx);
+	PRINT_RESULT("mqtt_disconnect", rc);
+
+	LOG_INF("Bye!");
+
+	return r;
+}
+
+static int start_app(void)
+{
+	int r = 0, i = 0;
+
+	while (!CONFIG_NET_SAMPLE_APP_MAX_CONNECTIONS ||
+		   i++ < CONFIG_NET_SAMPLE_APP_MAX_CONNECTIONS)
+	{
+		r = publisher();
+
+		if (!CONFIG_NET_SAMPLE_APP_MAX_CONNECTIONS)
+		{
+			k_sleep(K_MSEC(PROG_DELAY));
+		}
+	}
+
+	return r;
+}
+
+static void wifi_connect(void)
+{
+	struct net_if *iface = net_if_get_default();
+	static struct wifi_connect_req_params wifi_params = {
+		.ssid = WIFI_SSID,
+		.ssid_length = 0, // Will be calculated
+		.psk = WIFI_PSK,
+		.psk_length = 0, // Will be calculated
+		.channel = WIFI_CHANNEL_ANY,
+		.security = WIFI_SECURITY_TYPE_PSK,
+	};
+
+	// Calculate SSID and PSK lengths
+	wifi_params.ssid_length = strlen(wifi_params.ssid);
+	wifi_params.psk_length = strlen(wifi_params.psk);
+
+	LOG_INF("Connecting to SSID: %s...", wifi_params.ssid);
+
+	if (net_mgmt(NET_REQUEST_WIFI_CONNECT, iface,
+				 &wifi_params, sizeof(struct wifi_connect_req_params)))
+	{
+		LOG_ERR("Failed to request Wi-Fi connection");
+		return;
+	}
+}
+
+static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb,
+									uint32_t mgmt_event,
+									struct net_if *iface)
+{
+	switch (mgmt_event)
+	{
+	case NET_EVENT_WIFI_CONNECT_RESULT:
+		LOG_INF("Wi-Fi connected!");
+		break;
+	case NET_EVENT_WIFI_DISCONNECT_RESULT:
+		LOG_INF("Wi-Fi disconnected - reconnecting...");
+		wifi_connect();
+		break;
+	}
+}
+
+static struct net_mgmt_event_callback wifi_cb;
+
+static void setup_wifi(void)
+{
+	net_mgmt_init_event_callback(&wifi_cb,
+								 wifi_mgmt_event_handler,
+								 (NET_EVENT_WIFI_CONNECT_RESULT |
+								  NET_EVENT_WIFI_DISCONNECT_RESULT));
+	net_mgmt_add_event_callback(&wifi_cb);
+}
+
+int main(void)
+{
+	LOG_INF("Starting Wi-Fi connection...");
+	setup_wifi();
+	wifi_connect();
+
+	struct net_if *iface = net_if_get_default();
+	if (!iface)
+	{
+		LOG_ERR("No default network interface found!");
+		return -1;
+	}
+
+	exit(start_app());
+	return 0;
+}
