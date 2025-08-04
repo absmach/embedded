f21098d5acec6e2f5ea5672579a56b643ea39ad6
Rodney Osodo <socials@rodneyosodo.com>
2025-08-04 14:23:50 +0300

    feat(ws): implement websocket with supermq
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

---
+++ targets/zephyr/websocket/src/config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* STA Mode Configuration */
+#define WIFI_SSID "SSID"    // Replace `SSID` with WiFi ssid
+#define WIFI_PSK "PASSWORD" // Replace `PASSWORD` with Router password
+
+/* Magistrala Configuration */
+#define MAGISTRALA_IP                                                          \
+  "MAGISTRALA_IP" // Replace with your Magistrala instance IP
+#define MAGISTRALA_WS_PORT 8186
+#define DOMAIN_ID "DOMAIN_ID"         // Replace with your Domain ID
+#define CLIENT_ID "CLIENT_ID"         // Replace with your Client ID
+#define CLIENT_SECRET "CLIENT_SECRET" // Replace with your Client secret
+#define CHANNEL_ID "CHANNEL_ID"       // Replace with your Channel ID
+
+#endif
