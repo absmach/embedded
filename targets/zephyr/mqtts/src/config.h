e8ee06fb73b5611239349d24281121d2d02db8fa
Rodney Osodo <socials@rodneyosodo.com>
2025-10-28 15:46:55 +0300

    fix: test mqtts

--- targets/zephyr/mqtts/src/config.h
+++ targets/zephyr/mqtts/src/config.h
@@ -1,24 +1,26 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define MQTT_CLIENTID ""
-#define SNTP_SERVER "0.pool.ntp.org"
-#define BROKER ""
-#define BROKER_PORT "8883"
-#define MQTT_BUFFER_SIZE 256u
-#define APP_BUFFER_SIZE 4096u
-#define MAX_RETRIES 10u
-#define BACKOFF_EXP_BASE_MS 1000u
-#define BACKOFF_EXP_MAX_MS 60000u
-#define BACKOFF_CONST_MS 5000u
-#define KEEP_ALIVE 60
-#define TOPIC_BUFFER_SIZE 128
+/* STA Mode Configuration */
+#define WIFI_SSID "internal"    // Replace `SSID` with WiFi ssid
+#define WIFI_PSK "$$Osiepna2024" // Replace `PASSWORD` with Router password
 
-const char *mgThingId = " ";
-const char *mgThingKey = " ";
-const char *mgChannelId = " ";
-char mgTopic[TOPIC_BUFFER_SIZE];
+/* Mosquitto Test Broker Configuration */
+#define MOSQUITTO_BROKER_HOSTNAME "messaging.magistrala-beta.absmach.eu"
+#define MOSQUITTO_BROKER_PORT 8883  // Encrypted, unauthenticated MQTTS port
+#define MQTT_CLIENTID "zephyr_mqtts_client_new"  // Replace with unique client ID
+#define CLIENT_SECRET "f6883013-af94-44c4-bdef-7188089c05fd" // Replace with your Client secret
+#define CLIENT_ID "2fde3a36-e098-485c-8b83-314facaef903"         // Replace with your Client ID
 
-const char *brokername = ""
+/* MQTT Topic Configuration */
+#define MQTT_PUBLISH_TOPIC "m/49baf6fe-2f7c-4748-8e99-2846346ef6ba/c/0ffc71bc-4a25-4e22-bfb8-b1a847ed50ab/pub"
+#define MQTT_SUBSCRIBE_TOPIC "m/49baf6fe-2f7c-4748-8e99-2846346ef6ba/c/0ffc71bc-4a25-4e22-bfb8-b1a847ed50ab/sub"
+
+/* Application Configuration */
+#define APP_MQTT_BUFFER_SIZE 1024
+#define APP_CONNECT_TRIES 3
+#define APP_CONNECT_TIMEOUT_MS 5000
+#define APP_SLEEP_MSECS 1000
+#define TELEMETRY_INTERVAL_SEC 30
 
 #endif
