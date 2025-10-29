066754371a72c38a8917b41343afe16f6a117e62
Rodney Osodo <socials@rodneyosodo.com>
2025-10-29 15:38:16 +0300

    fix(https): test locally and staging

--- targets/zephyr/https/src/config.h
+++ targets/zephyr/https/src/config.h
@@ -2,15 +2,15 @@
 #define CONFIG_H
 
 /* STA Mode Configuration */
-#define WIFI_SSID "internal"    // Replace `SSID` with WiFi ssid
-#define WIFI_PSK "$$Osiepna2024" // Replace `PASSWORD` with Router password
+#define WIFI_SSID "SSID"    // Replace `SSID` with WiFi ssid
+#define WIFI_PSK "PASSWORD" // Replace `PASSWORD` with Router password
 
 /* Magistrala Configuration */
-#define MAGISTRALA_HOSTNAME "messaging.magistrala-beta.absmach.eu" // Replace with your Magistrala instance hostname
+#define MAGISTRALA_HOSTNAME "MAGISTRALA_HOSTNAME" // Replace with your Magistrala instance hostname or IP
 #define MAGISTRALA_HTTPS_PORT 443
-#define DOMAIN_ID "49baf6fe-2f7c-4748-8e99-2846346ef6ba"         // Replace with your Domain ID
-#define CLIENT_ID "2fde3a36-e098-485c-8b83-314facaef903"         // Replace with your Client ID
-#define CLIENT_SECRET "f6883013-af94-44c4-bdef-7188089c05fd" // Replace with your Client secret
-#define CHANNEL_ID "0ffc71bc-4a25-4e22-bfb8-b1a847ed50ab"       // Replace with your Channel ID
+#define DOMAIN_ID "DOMAIN_ID"         // Replace with your Domain ID
+#define CLIENT_ID "CLIENT_ID"         // Replace with your Client ID
+#define CLIENT_SECRET "CLIENT_SECRET" // Replace with your Client secret
+#define CHANNEL_ID "CHANNEL_ID"       // Replace with your Channel ID
 
 #endif
--- targets/zephyr/https/src/main.c
+++ targets/zephyr/https/src/main.c
@@ -3,6 +3,7 @@
 #include <zephyr/net/net_mgmt.h>
 #include <zephyr/net/socket.h>
 #include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/sys/reboot.h>
 
 #include "ca_cert.h"
 #include "config.h"
@@ -22,7 +23,8 @@ static struct net_if *sta_iface;
 
 #define TELEMETRY_INTERVAL_SEC 30
 
-typedef struct {
+typedef struct
+{
   double temperature;
   double humidity;
   int battery_level;
@@ -46,19 +48,23 @@ static uint8_t recv_buf_ipv6[MAX_RECV_BUF_LEN];
 #define TLS_SEC_TAG 1
 
 static void net_mgmt_event_handler(struct net_mgmt_event_callback *cb,
-                                   uint32_t mgmt_event, struct net_if *iface) {
-  if (mgmt_event == NET_EVENT_IPV4_DHCP_BOUND) {
+                                   uint32_t mgmt_event, struct net_if *iface)
+{
+  if (mgmt_event == NET_EVENT_IPV4_DHCP_BOUND)
+  {
     LOG_INF("DHCP bound - got IP address");
     k_sem_give(&dhcp_sem);
   }
 }
 
-static int wait_for_ip_address(struct net_if *iface) {
+static int wait_for_ip_address(struct net_if *iface)
+{
   struct net_if_addr *if_addr;
   const int max_timeout = 30; // 30 seconds max wait
 
   if_addr = net_if_ipv4_get_global_addr(iface, NET_ADDR_PREFERRED);
-  if (if_addr) {
+  if (if_addr)
+  {
     char addr_str[NET_IPV4_ADDR_LEN];
     net_addr_ntop(AF_INET, &if_addr->address.in_addr, addr_str,
                   sizeof(addr_str));
@@ -69,18 +75,21 @@ static int wait_for_ip_address(struct net_if *iface) {
   LOG_INF("Waiting for IP address via DHCP...");
 
   int ret = k_sem_take(&dhcp_sem, K_SECONDS(max_timeout));
-  if (ret != 0) {
+  if (ret != 0)
+  {
     LOG_ERR("Timeout waiting for DHCP - checking if we have IP anyway");
 
     if_addr = net_if_ipv4_get_global_addr(iface, NET_ADDR_PREFERRED);
-    if (!if_addr) {
+    if (!if_addr)
+    {
       LOG_ERR("No IP address available");
       return -ETIMEDOUT;
     }
   }
 
   if_addr = net_if_ipv4_get_global_addr(iface, NET_ADDR_PREFERRED);
-  if (if_addr) {
+  if (if_addr)
+  {
     char addr_str[NET_IPV4_ADDR_LEN];
     net_addr_ntop(AF_INET, &if_addr->address.in_addr, addr_str,
                   sizeof(addr_str));
@@ -92,12 +101,14 @@ static int wait_for_ip_address(struct net_if *iface) {
   return -1;
 }
 
-static int setup_tls_credentials(void) {
+static int setup_tls_credentials(void)
+{
   int ret;
 
   ret = tls_credential_add(TLS_SEC_TAG, TLS_CREDENTIAL_CA_CERTIFICATE, ca_cert,
                            sizeof(ca_cert));
-  if (ret < 0) {
+  if (ret < 0)
+  {
     LOG_ERR("Failed to add CA certificate: %d", ret);
     return ret;
   }
@@ -107,7 +118,8 @@ static int setup_tls_credentials(void) {
 }
 
 static int resolve_hostname(const char *hostname,
-                           struct sockaddr_storage *addr) {
+                            struct sockaddr_storage *addr)
+{
   int ret;
   struct addrinfo *result;
   struct addrinfo hints = {
@@ -118,7 +130,8 @@ static int resolve_hostname(const char *hostname,
   LOG_INF("Resolving %s...", hostname);
 
   ret = getaddrinfo(hostname, NULL, &hints, &result);
-  if (ret != 0) {
+  if (ret != 0)
+  {
     LOG_ERR("getaddrinfo failed: %d", ret);
     return -EINVAL;
   }
@@ -134,12 +147,14 @@ static int resolve_hostname(const char *hostname,
   return 0;
 }
 
-static int setup_socket(int *sock) {
+static int setup_socket(int *sock)
+{
   int ret = 0;
 
   *sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TLS_1_2);
 
-  if (*sock < 0) {
+  if (*sock < 0)
+  {
     LOG_ERR("Failed to create HTTPS socket (%d)", -errno);
     return -errno;
   }
@@ -148,7 +163,8 @@ static int setup_socket(int *sock) {
   sec_tag_t sec_tag_opt[] = {TLS_SEC_TAG};
   ret = setsockopt(*sock, SOL_TLS, TLS_SEC_TAG_LIST, sec_tag_opt,
                    sizeof(sec_tag_opt));
-  if (ret < 0) {
+  if (ret < 0)
+  {
     LOG_ERR("Failed to set TLS_SEC_TAG option: %d", errno);
     close(*sock);
     return -errno;
@@ -157,7 +173,8 @@ static int setup_socket(int *sock) {
   /* Set hostname for TLS SNI */
   ret = setsockopt(*sock, SOL_TLS, TLS_HOSTNAME, MAGISTRALA_HOSTNAME,
                    strlen(MAGISTRALA_HOSTNAME));
-  if (ret < 0) {
+  if (ret < 0)
+  {
     LOG_ERR("Failed to set TLS_HOSTNAME option: %d", errno);
     close(*sock);
     return -errno;
@@ -166,7 +183,8 @@ static int setup_socket(int *sock) {
   /* Set peer verification */
   int verify = TLS_PEER_VERIFY_REQUIRED;
   ret = setsockopt(*sock, SOL_TLS, TLS_PEER_VERIFY, &verify, sizeof(verify));
-  if (ret < 0) {
+  if (ret < 0)
+  {
     LOG_ERR("Failed to set TLS_PEER_VERIFY option: %d", errno);
     close(*sock);
     return -errno;
@@ -177,12 +195,14 @@ static int setup_socket(int *sock) {
   return ret;
 }
 
-static int payload_cb(int sock, struct http_request *req, void *user_data) {
+static int payload_cb(int sock, struct http_request *req, void *user_data)
+{
   const char *content[] = {"foobar", "chunked", "last"};
   char tmp[64];
   int i, pos = 0;
 
-  for (i = 0; i < ARRAY_SIZE(content); i++) {
+  for (i = 0; i < ARRAY_SIZE(content); i++)
+  {
     pos += snprintk(tmp + pos, sizeof(tmp) - pos, "%x\r\n%s\r\n",
                     (unsigned int)strlen(content[i]), content[i]);
   }
@@ -195,10 +215,14 @@ static int payload_cb(int sock, struct http_request *req, void *user_data) {
 }
 
 static int response_cb(struct http_response *rsp,
-                       enum http_final_call final_data, void *user_data) {
-  if (final_data == HTTP_DATA_MORE) {
+                       enum http_final_call final_data, void *user_data)
+{
+  if (final_data == HTTP_DATA_MORE)
+  {
     LOG_INF("Partial data received (%zd bytes)", rsp->data_len);
-  } else if (final_data == HTTP_DATA_FINAL) {
+  }
+  else if (final_data == HTTP_DATA_FINAL)
+  {
     LOG_INF("All the data received (%zd bytes)", rsp->data_len);
   }
 
@@ -208,11 +232,13 @@ static int response_cb(struct http_response *rsp,
   return 0;
 }
 
-static int connect_socket(int *sock, struct sockaddr_storage *addr) {
+static int connect_socket(int *sock, struct sockaddr_storage *addr)
+{
   int ret;
 
   ret = setup_socket(sock);
-  if (ret < 0 || *sock < 0) {
+  if (ret < 0 || *sock < 0)
+  {
     return -1;
   }
 
@@ -223,33 +249,45 @@ static int connect_socket(int *sock, struct sockaddr_storage *addr) {
   LOG_INF("Connecting to %s:%d...", addr_str, ntohs(addr4->sin_port));
 
   ret = connect(*sock, (struct sockaddr *)addr, sizeof(struct sockaddr_in));
-  if (ret < 0) {
+  if (ret < 0)
+  {
     LOG_ERR("Cannot connect to remote (%d)", -errno);
     close(*sock);
     *sock = -1;
     ret = -errno;
-  } else {
+  }
+  else
+  {
     LOG_INF("Connected successfully via TLS");
   }
 
   return ret;
 }
 
-static int run_queries(void) {
+static void update_sensor_data(sensor_data_t *data)
+{
+  data->temperature = 20.0 + (sys_rand32_get() % 1000) / 100.0; // 20-30°C
+  data->humidity = 50.0 + (sys_rand32_get() % 3000) / 100.0;    // 50-80%
+  data->battery_level = 70 + (sys_rand32_get() % 30);           // 70-99%
+  data->led_state = !data->led_state;                           // Toggle LED
+}
+
+static int run_queries(void)
+{
   static struct sockaddr_storage server_addr;
   static bool addr_resolved = false;
   int sock = -1;
   int32_t timeout = 3 * MSEC_PER_SEC;
   int ret = 0;
 
-  /* Resolve hostname once and cache it */
-  if (!addr_resolved) {
+  if (!addr_resolved)
+  {
     ret = resolve_hostname(MAGISTRALA_HOSTNAME, &server_addr);
-    if (ret < 0) {
+    if (ret < 0)
+    {
       LOG_ERR("Failed to resolve hostname: %d", ret);
       return ret;
     }
-    /* Set port after resolution */
     ((struct sockaddr_in *)&server_addr)->sin_port =
         htons(MAGISTRALA_HTTPS_PORT);
     addr_resolved = true;
@@ -257,19 +295,31 @@ static int run_queries(void) {
 
   /* Create socket and connect */
   ret = connect_socket(&sock, &server_addr);
-  if (ret < 0 || sock < 0) {
+  if (ret < 0 || sock < 0)
+  {
     LOG_ERR("Cannot create HTTPS connection for SenML telemetry.");
     return -ECONNABORTED;
   }
 
-  /* Prepare and send HTTP request */
+  update_sensor_data(&current_data);
+
   struct http_request req;
 
-   const char *senml_payload = "[{\"bn\":\"some-base-name:\",\"bt\":1."
-                                "276020076001e+09,\"bu\":\"A\",\"bver\":5,"
-                                "\"n\":\"voltage\",\"u\":\"V\",\"v\":120.1},"
-                                "{\"n\":\"current\",\"t\":-5,\"v\":1.2},"
-                                "{\"n\":\"current\",\"t\":-4,\"v\":1.3}]";
+  int64_t unix_time = k_uptime_get() / 1000 + 1761700000; // Approximate Unix time
+
+  char senml_payload[512];
+  snprintf(senml_payload, sizeof(senml_payload),
+           "[{\"bn\":\"esp32s3:\",\"bt\":%lld,\"bu\":\"Cel\",\"bver\":5,"
+           "\"n\":\"temperature\",\"u\":\"Cel\",\"v\":%.2f},"
+           "{\"n\":\"humidity\",\"u\":\"%%RH\",\"v\":%.2f},"
+           "{\"n\":\"battery\",\"u\":\"%%\",\"v\":%d},"
+           "{\"n\":\"led\",\"vb\":%s}]",
+           unix_time, current_data.temperature, current_data.humidity,
+           current_data.battery_level, current_data.led_state ? "true" : "false");
+
+  LOG_INF("Sending telemetry: temp=%.2f°C, humidity=%.2f%%, battery=%d%%, led=%s, time=%lld",
+          current_data.temperature, current_data.humidity, current_data.battery_level,
+          current_data.led_state ? "ON" : "OFF", unix_time);
 
   char auth_header[128];
   snprintf(auth_header, sizeof(auth_header), "Authorization: Client %s\r\n",
@@ -301,10 +351,12 @@ static int run_queries(void) {
   return ret;
 }
 
-static int start_app(void) {
+static int start_app(void)
+{
   int r = 0;
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; i++)
+  {
     r = run_queries();
     k_sleep(K_SECONDS(TELEMETRY_INTERVAL_SEC));
   }
@@ -312,7 +364,8 @@ static int start_app(void) {
   return r;
 }
 
-int main(void) {
+int main(void)
+{
   LOG_INF("Magistrala HTTPS Client Starting");
 
   k_sleep(K_SECONDS(5));
@@ -326,29 +379,41 @@ int main(void) {
   net_mgmt_add_event_callback(&mgmt_cb);
 
   sta_iface = net_if_get_wifi_sta();
-  if (sta_iface == NULL) {
+  if (sta_iface == NULL)
+  {
     LOG_ERR("Failed to get WiFi STA interface");
+    LOG_ERR("Restarting system in 5 seconds...");
+    k_sleep(K_SECONDS(5));
+    sys_reboot(SYS_REBOOT_COLD);
     return -ENODEV;
   }
 
   int ret = connect_to_wifi(sta_iface, WIFI_SSID, WIFI_PSK);
-  if (ret) {
+  if (ret)
+  {
     LOG_ERR("Unable to Connect to (%s)", WIFI_SSID);
+    LOG_ERR("Restarting system in 5 seconds...");
+    k_sleep(K_SECONDS(5));
+    sys_reboot(SYS_REBOOT_COLD);
     return ret;
   }
 
   ret = wait_for_ip_address(sta_iface);
-  if (ret < 0) {
+  if (ret < 0)
+  {
     LOG_ERR("Failed to get IP address: %d", ret);
+    LOG_ERR("Restarting system in 5 seconds...");
+    k_sleep(K_SECONDS(5));
+    sys_reboot(SYS_REBOOT_COLD);
     return ret;
   }
 
   /* Add a small delay to ensure network is fully ready */
   k_sleep(K_SECONDS(2));
 
-  /* Setup TLS credentials */
   ret = setup_tls_credentials();
-  if (ret < 0) {
+  if (ret < 0)
+  {
     LOG_ERR("Failed to setup TLS credentials: %d", ret);
     return ret;
   }
